ca4a17d5774a4b065b023893db0b1b65c27d0747
Siddhi Goswami <gosiddhi03@gmail.com>
2018-07-10 21:22:44 -0500

    07/10

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+export default `
+<div id="footer">
+  <ul>
+    <li id="boldlinky">
+      <a href="https://www.instagram.com/siddhi_goswami/?hl=en">
+        <i class="fab fa-instagram"></i>
+        Instagram</a>
+    </li>
+    <li id="boldlinky">
+     <a href="https://github.com/siddhigoswami">
+      <i class="fab fa-github"></i>
+      GitHub</a> 
+    </li>
+  </ul>
+  </div>
+</div>
+</div>
+`;
---
+++ src/Navigation.js
@@ -0,0 +1,18 @@
+export default `
+<div id="navigation">
+   <ul class="container">
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+      <li>
+        <a href="/Projects">Projects</a> 
+     <ul class="dropdown">
+         <li>first</li>
+         <li>second</li>
+         <li>third</li>
+       </ul>
+      </li>
+      
+      </ul>
+  </div>
+`;
